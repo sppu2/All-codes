6d38c78c0c0f1cdc8b97488bfd39d0024b79a953
sppu2 <121567163+sppu2@users.noreply.github.com>
2023-01-03 01:58:46 +0530

    Update 1) Average Score.py

--- 1) Average Score.py
+++ 1) Average Score.py
@@ -1,110 +1,73 @@
-# Group A = group of students who play cricket
-# Group B = group of students who play badminton
-# Group C = group of students who play football
+marks=[]
+def stud_get():
+    print("Enter Number oF Students In SE Class: ")
+    N = int(input())
+    global marks
+    for i in range(N):
+        print("Enter the Marks(Write -1 for Absent Students)")
+        M = int(input())
+        marks.append(M)
+    print(marks)
 
-groupA = []
-groupB = []
-groupC = []
-uni = []
+def avg_marks():
+    sum=0
+    cnt=0
+    for i in range(len(marks)):
+        if marks[i] != -1:
+            sum=sum+marks[i]
+            cnt+=1
+    print("Total Marks= ",sum)
+    print("Average in Float= ", sum/cnt)
+    print("Average in Integer= ", sum//cnt)
 
-def get():
-    print("Enter Number of Students who Play Cricket: ")
-    C = int(input())
-    global groupA
-    print("Enter the Roll Number who play Cricket: ")
-    for i in range(C):
-        cri=int(input("Enter Roll Number: "))
-        groupA.append(cri)
-    print(groupA)
-    global groupB
-    print("Enter the Roll Number who play Badminton: ")
-    B = int(input())
-    for i in range(B):
-        bad = int(input("Enter Roll Number: "))
-        groupB.append(bad)
-    print(groupB)
-    global groupC
-    print("Enter the Roll Number who play Football: ")
-    F = int(input())
-    for i in range(F):
-        foot = int(input("Enter Roll Number: "))
-        groupA.append(foot)
-    print(groupC)
-    print("Enter Number of Students in Class: ")
-    U = int(input())
-    global uni
-    for i in range(U):
-        uni.append(i)
-    print(uni)
+def high_low():
+    temp=marks[0]
+    for i in range(len(marks)):
+        if temp < marks[i]:
+            temp=marks[i]
+    print("Highest Marks= ",temp)
+    temp=marks[0]
+    for i in range(len(marks)):
+        if marks[i] != -1:
+            if temp>marks[i]:
+                temp=marks[i]
+    print("Lowest Marks= ",temp)
 
-# List of students who play both cricket and badminton
-def cri_bad():
-    uni_cri_bad=[]
-    for i in groupA:
-        for j in groupB:
-            if i==j:
-                uni_cri_bad.append(j)
-    print(uni_cri_bad)
+def count_abs():
+    cnt=0
+    for i in range(len(marks)):
+        if marks[i] == -1:
+            cnt+=1
+    print("Number of Students Absent: ",cnt)
 
-# list of students who play either cricket or badminton but not both
-def cri_bad_not_both():
-    not_cri_bad = []
-    for i in groupA:
-        if i not in groupB:
-            not_cri_bad.append(i)
-    for j in groupB:
-        if j not in groupA:
-            not_cri_bad.append(j)
-    print(not_cri_bad)
+def high_freq():
+    freq=[]
+    for i in range(len(marks)):
+        if marks[i] != -1:
+            freq.append(marks.count(marks[i]))
+    print(freq)
+    k=max(freq)
+    print("Highest Frequency+ ",k)
+    print("Highest Marks= ",marks[k])
 
-# list of students who play neither cricket nor badminton
-def nei_cri_nor_bad():
-    uni_cri_bad = []
-    uni_cri_bad.extend(groupA)
-    for j in groupB:
-        if j not in groupA:
-            uni_cri_bad.append(j)
-    print(uni_cri_bad)
-    # comparing this to uni array
-    not_cri_bad = []
-    for i in uni:
-        if i not in uni_cri_bad:
-            not_cri_bad.append(i)
-    print(not_cri_bad)
-
-# list of students who play cricket football and not badminton
-def cri_foot_not_bad():
-    cri_foot = []
-    cri_foot.extend(groupA)
-    for i in groupC:
-        if i not in groupA:
-            cri_foot.append(i)
-    final=[]
-    for j in cri_foot:
-        if j not in groupB:
-            final.append(j)
-    print(final)
 if __name__ == '__main__':
-    print("***** Take Input *****")
-    get()
+    print("*****TAKE INPUT*****")
+    stud_get()
     while(True):
-        print("1. List of Students who play both Cricket And Badminton: ")
-        print("2. List of Students who play either Cricket Or Badminton but Not Both: ")
-        print("3. List of Students who play neither Cricket nor Badminton but Not Both: ")
-        print("4. List of students who play Cricket And Badminton but not Badminton: ")
-        print("5. Exit")
-        print("Enter Choice ")
+        print("1. The Average Score of Class: ")
+        print("2. Highest Score and Lowest Score of Class: ")
+        print("3. Count of Students who were Absent for the Test: ")
+        print("4. Display Marks with Highest frequency: ")
+        print("5. EXIT")
+        print("Enter Choice")
         choice=int(input())
         if(choice==1):
-            cri_bad()
+            avg_marks()
         if(choice==2):
-            cri_bad_not_both()
-        if(choice==3):
-            nei_cri_nor_bad()
-        if(choice==4):
-            cri_foot_not_bad()
-        if(choice==5):
+            high_low()
+        if (choice == 3):
+            count_abs()
+        if (choice == 4):
+            high_freq()
+        if (choice == 5):
             exit()
-
-
-
